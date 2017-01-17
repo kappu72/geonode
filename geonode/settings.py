15843ebc69985e14afb8237c0ce05f03945b03ab
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2017-01-17 12:24:19 +0100

    Added edit metadata wizard
     - First version of a wizard to edit metadata
     - Separated tab for preview

--- geonode/settings.py
+++ geonode/settings.py
@@ -1063,4 +1063,6 @@ if 'geonode.geoserver' in INSTALLED_APPS:
 # e.g. THESAURI = [{'name':'inspire_themes', 'required':True, 'filter':True}, {'name':'inspire_concepts', 'filter':True}, ]
 # Required: (boolean, optional, default false) mandatory while editing metadata (not implemented yet)
 # Filter: (boolean, optional, default false) a filter option on that thesaurus will appear in the main search page
-THESAURI = [{'name':'inspire_themes', 'required':True, 'filter':True}]
+# THESAURI = [{'name':'inspire_themes', 'required':True, 'filter':True}]
+THESAURI = []
+
